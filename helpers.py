3a1895fd0c8d9a642d33316b463cc8aad8058113
Rob Wilson <roobert@gmail.com>
2019-09-02 23:53:13 +0100

    correct message length

--- helpers.py
+++ helpers.py
@@ -9,10 +9,8 @@ def xor(buf_x_bytes, buf_y_bytes):
 def repeating_xor(message, key):
     """binary xor two sets of bytes where the second set of bytes is a repeated key"""
 
-    message_length = len(message)
+    message_length = len(message)+1
 
     repeated_key = (key * message_length)[:message_length]
-    print(message)
-    print(repeated_key)
 
-    return bytes(x ^ y for x, y in zip(message.encode("ASCII"), repeated_key.encode("ASCII")))
+    return bytes(x ^ y for x, y in zip(message, repeated_key))
