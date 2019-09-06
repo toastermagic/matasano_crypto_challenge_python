05fdb65e339151dce783f16dc760879105ee3986
Rob Wilson <roobert@gmail.com>
2019-09-06 20:57:35 +0100

    detect AES ECB

---
+++ set1/test_challenge8.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('lib')
+
+import unittest
+from pprint import pprint
+from Crypto.Cipher import AES
+from base64 import b64decode
+from iteration_utilities import grouper
+
+class TestChallenge8(unittest.TestCase):
+    with open("data/8.txt", encoding="ISO-8859-1") as file:
+        data = file.readlines()
+
+    target = "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a\n"
+
+    def test_set8(self):
+        likely_ecb_texts = []
+        line_number = 0
+
+        # for each line in the file, work out how many repeated chunks are in the file.
+        # lots of repeated chunks means its likely ecb mode..
+        for line in self.data:
+            line_number += 1
+
+            chunks = {}
+
+            # split line into 16 byte chunks since key is 16 bytes long
+            for chunk in list(grouper(b64decode(line), 16)):
+                if tuple(chunk) in chunks.keys():
+                    chunks[tuple(chunk)] += 1
+                else:
+                    chunks[tuple(chunk)] = 0
+
+            repeated_chunk_total = sum(chunks.values())
+
+            if repeated_chunk_total == 0:
+                continue
+
+            likely_ecb_texts.append({
+                "line_number": line_number,
+                "cipher_text": line,
+                "repeated_chunk_total": repeated_chunk_total
+            })
+
+        #pprint(likely_ecb_texts)
+
+        self.assertEqual(likely_ecb_data[0]['cipher_text'], self.target)
+        self.assertEqual(likely_ecb_data[0]['line_number'], 133)
+        self.assertEqual(likely_ecb_data[0]['repeated_chunk_total'], 3)
+
+if __name__ == '__main__':
+    unittest.main()
