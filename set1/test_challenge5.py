adc67c3f035e04bd4b613c47a31b1d07a59cfbba
Rob Wilson <roobert@gmail.com>
2019-09-06 21:34:14 +0100

    formatting

--- set1/test_challenge5.py
+++ set1/test_challenge5.py
@@ -11,12 +11,16 @@ from xor import *
 class TestChallenge5(unittest.TestCase):
     message = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 
-    target = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    target = "0b3637272a2b2e63622c2e69692a23693a2a3c6324" \
+            + "202d623d63343c2a26226324272765272a282b2f20" \
+            + "430a652e2c652a3124333a653e2b2027630c692b20" \
+            + "283165286326302e27282f"
 
     def test_challenge5(self):
-        hex = hexlify(repeating_xor(self.message.encode("ASCII"), "ICE".encode("ASCII"))).decode("ASCII")
+        hex_bytes = hexlify(repeating_xor(self.message.encode("ASCII"), \
+                "ICE".encode("ASCII")))
 
-        self.assertEqual(self.target, hex)
+        self.assertEqual(self.target, hex_bytes.decode("ASCII"))
 
 if __name__ == '__main__':
     unittest.main()
