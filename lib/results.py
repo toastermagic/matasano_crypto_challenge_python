79a8602ee5a3431d13bc1ccb072b7e7828571474
Rob Wilson <roobert@gmail.com>
2019-09-07 13:13:55 +0100

    switch env to python3

--- lib/results.py
+++ lib/results.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 def find_max(data, key):
     return max(data, key=lambda x:x[key])
--- set1/test_challenge1.py
+++ set1/test_challenge1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # 1. Convert hex to base64 and back.
 #
