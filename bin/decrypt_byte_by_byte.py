b6bd58a11982c747bc78afbc403d93be2ea847c8
Rob Wilson <roobert@gmail.com>
2019-09-14 21:27:54 +0100

    fun output for byte by byte decryption

---
+++ bin/decrypt_byte_by_byte.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# for fun!
+
+import sys
+sys.path.append('../lib')
+
+import unittest
+import time
+import re
+from pprint import pprint
+from random import randint
+from base64 import *
+from ecb import *
+
+secret = b64decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg" \
+       + "aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq" \
+       + "dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg" \
+       + "YnkK")
+
+# consistent but *unknown* key
+with open("../set2/data/12-key.txt", encoding="ISO-8859-1") as file:
+    key = b64decode(file.read())
+
+oracle_context = {
+    "key": key,
+    "secret": secret
+}
+
+print()
+print("  ECB byte by byte decryption ")
+print()
+
+plain_text = break_ecb_byte_by_byte(oracle_context, True).decode("ASCII")
+
+print("\r  * decrypting: done!                  ")
+print()
+print("  * decrypted cipher text:")
+print()
+
+for line in plain_text.splitlines():
+    print("      " + line)
